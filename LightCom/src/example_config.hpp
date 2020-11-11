c173f572e58b30820d4a5c7488ccb0f4fcbe8b53
Universum <programming@universegame.de>
2020-11-11 10:09:21 +0100

    updated example_config.hpp

--- LightCom/src/example_config.hpp
+++ LightCom/src/example_config.hpp
@@ -1,6 +1,7 @@
 /*
 this is an example config
 if you've cloned this project and no config.hpp exists, rename this file and change the settings for your need
+if you constribute to this project and you don't have a config.hpp copy this file and rename it to "config.hpp" instead of just renaming this file, so the example_config.hhpp wont't be lost
 */
 
 #pragma once
