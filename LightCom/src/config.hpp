d706f321a1ebdb740a8af45e6b06f76994365691
Universum <programming@universegame.de>
2020-11-11 10:07:40 +0100

    updated example config and gitignore

--- LightCom/src/config.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define BAUD_RATE 115200
--- LightCom/src/example_config.hpp
+++ LightCom/src/example_config.hpp
@@ -3,6 +3,6 @@ this is an example config
 if you've cloned this project and no config.hpp exists, rename this file and change the settings for your need
 */
 
-
-
 #pragma once
+
+#define BAUD_RATE 115200
