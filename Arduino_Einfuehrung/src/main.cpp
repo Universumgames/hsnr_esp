5d7b54aea5944b1432bdfdc3964bb5793805ab2d
Universum <programming@universegame.de>
2020-11-17 14:16:48 +0100

    finished second task

--- Arduino_Einfuehrung/src/main.cpp
+++ Arduino_Einfuehrung/src/main.cpp
@@ -4,26 +4,49 @@
 #define red 6
 #define green 3
 #define yellow 5
-//subsonic sensor pins
-#define trigger 2
+//supersonic sensor pins
+#define trigger 2 
 #define echo 4
 //potentiometer
 #define pot A0
 
-void setup() {
+void allLED(bool value)
+{
+  digitalWrite(red, value);
+  digitalWrite(green, value);
+  digitalWrite(yellow, value);
+}
+
+void setLED(int value)
+{
+  allLED(LOW);
+  if (value >= 0 && value <= 341)
+  {
+    digitalWrite(green, HIGH);
+  }
+  else if (value > 341 && value <= 682)
+  {
+    digitalWrite(yellow, HIGH);
+  }
+  else if (value > 682)
+  {
+    digitalWrite(red, HIGH);
+  }
+}
+
+void setup()
+{
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
-  
+
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
 
   pinMode(pot, INPUT);
 }
 
-void loop() {
-  digitalWrite(3, HIGH);
-  delay(100);
-  digitalWrite(3, LOW);
-  delay(100);
+void loop()
+{
+  setLED(analogRead(pot));
 }
