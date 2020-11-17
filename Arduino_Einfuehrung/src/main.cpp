3fe42ad4088ac5c8954014b07e856b8068948321
Universum <programming@universegame.de>
2020-11-17 17:28:51 +0100

    added comments, finished task 4 or something, cleaned up

--- Arduino_Einfuehrung/src/main.cpp
+++ Arduino_Einfuehrung/src/main.cpp
@@ -4,17 +4,26 @@
 #define RED 6
 #define GREEN 3
 #define YELLOW 5
-//supersonic sensor pins
+//ultrasonic sensor pins
 #define TRIGGER 2
 #define ECHO 4
 //potentiometer
 #define POT A0
+//led offset
+#define GREEN_END 400
+#define GREEN_BEGIN 250
+#define YELLOW_END 300
+#define YELLOW_BEGIN 150
+#define RED_END 200
+#define RED_BEGIN 0
 
+//if serial output is needed uncomment next line
 //#define SERIAL_DEBUG true
 
 //1 = true = HIGH
 //0 = false = LOW
 
+//set value for all LEDS
 void allLED(bool value)
 {
   digitalWrite(RED, value);
@@ -33,12 +42,13 @@ void setSimpleLED(int value)
   {
     digitalWrite(YELLOW, HIGH);
   }
-  else if (value > 200 )
+  else if (value > 200)
   {
     digitalWrite(RED, HIGH);
   }
 }
 
+//get Distance from ultrasonic Sensor
 double getDistance()
 {
   long time = 0;
@@ -50,19 +60,19 @@ double getDistance()
   digitalWrite(TRIGGER, LOW);
   time = pulseIn(ECHO, HIGH);
   distance = time * 0.017;
-  //if (distance <= 40)
-    return distance;
-  //else
-    //return -1;
+  return distance;
 }
 
 void setup()
 {
+  //initialise Serial if needed
 #ifdef SERIAL_DEBUG
   Serial.begin(9600);
   delay(500);
+  Serial.println("G\t Y\t R");
 #endif
 
+//define pin usage
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(YELLOW, OUTPUT);
@@ -71,22 +81,58 @@ void setup()
   pinMode(ECHO, INPUT);
 
   pinMode(POT, INPUT);
-
 }
 
 void loop()
 {
+  //turn all LEDs off
+  allLED(LOW);
   double dist = getDistance();
+
+//Smooth transition between three leds
+  double mappedVal = map(dist, 0, 37, 0, 370);
+  //red
+  if (mappedVal >= RED_BEGIN && mappedVal <= RED_END)
+  {
+    double val = mappedVal - RED_BEGIN;
+    double m = map(val, 0, RED_END - RED_BEGIN, 0, 255);
+    if (val > 0)
+      analogWrite(RED, m);
 #ifdef SERIAL_DEBUG
-  
-  String s = String(dist) + "cm";
-  Serial.println((dist != -1) ? s : "Distance too large");
-// same as
-//if (dist != -1)
-//    Serial.println(s);
-//  else
-//    Serial.println("Distance too large");
+    Serial.print(m);
+#endif
+  }
+#ifdef SERIAL_DEBUG
+  Serial.print("\t");
 #endif
 
-  setSimpleLED(map(dist, 0, 37, 0, 370));
+//yellow
+  if (mappedVal >= YELLOW_BEGIN && mappedVal <= YELLOW_END)
+  {
+    double val = mappedVal - YELLOW_BEGIN;
+    double m = map(val, 0, YELLOW_END - YELLOW_BEGIN, 0, 255);
+    if (val > 0)
+      analogWrite(YELLOW, m);
+#ifdef SERIAL_DEBUG
+    Serial.print(m);
+#endif
+  }
+#ifdef SERIAL_DEBUG
+  Serial.print("\t");
+#endif
+
+//green
+  if (mappedVal >= GREEN_BEGIN)
+  {
+    double val = mappedVal - GREEN_BEGIN;
+    double m = map(val, 0, GREEN_END - GREEN_BEGIN, 0, 255);
+    if (val > 0)
+      analogWrite(GREEN, m);
+#ifdef SERIAL_DEBUG
+    Serial.print(m);
+#endif
+  }
+#ifdef SERIAL_DEBUG
+  Serial.println();
+#endif
 }
