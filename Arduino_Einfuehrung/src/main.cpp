a8808372ca689771f70b20df104d036fc12a0fff
Universum <programming@universegame.de>
2020-11-17 12:40:25 +0100

    added definitions

--- Arduino_Einfuehrung/src/main.cpp
+++ Arduino_Einfuehrung/src/main.cpp
@@ -1,7 +1,24 @@
 #include <Arduino.h>
 
+//led pins
+#define red 6
+#define green 3
+#define yellow 5
+//subsonic sensor pins
+#define trigger 2
+#define echo 4
+//potentiometer
+#define pot A0
+
 void setup() {
-  pinMode(3, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  pinMode(pot, INPUT);
 }
 
 void loop() {
