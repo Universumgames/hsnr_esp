f62ac919a2a3c26cd548680305a3df90944f29e5
Universum <programming@universegame.de>
2021-01-03 22:56:14 +0100

    added proper commenting and formatted code

--- LightCom/LightCom.ino
+++ LightCom/LightCom.ino
@@ -1,75 +1,96 @@
+//declare variables
 int sender_clock = 6;
 int sender_data = 7;
-int reciever_clock= 4;
-int reciever_data= 3;
+int reciever_clock = 4;
+int reciever_data = 3;
 int incomingByte = 0;
 int reciever_clock_2 = 2;
-long long last_millis = 0; 
+long long last_millis = 0;
 byte temp;
 int recieve_index = 7;
 byte b;
 
-
-
-void setup(){  
-
-  attachInterrupt(digitalPinToInterrupt(reciever_clock),clock_interrupt_start,FALLING);
-  attachInterrupt(digitalPinToInterrupt(reciever_clock),clock_interrupt_end,RISING);
+void setup()
+{
+  //attach Interrupts to not having to worry about reading from one pin
+  attachInterrupt(digitalPinToInterrupt(reciever_clock), clock_interrupt_start, FALLING);
+  attachInterrupt(digitalPinToInterrupt(reciever_clock), clock_interrupt_end, RISING);
+  //initialize Serial Monitor
   Serial.begin(9600);
+  //set pin modes
   pinMode(sender_clock, OUTPUT);
   pinMode(sender_data, OUTPUT);
   pinMode(reciever_clock, INPUT);
   pinMode(reciever_data, INPUT);
   pinMode(reciever_clock_2, INPUT);
-  
-  
 }
 
 void loop()
 {
-if (Serial.available() > 0) {
+  //check if serial queue contains items
+  if (Serial.available() > 0)
+  {
+    //read incoming byte (charactere)
     incomingByte = Serial.read();
- for (int i = 7; i >=0; i--){
-     Serial.print(bitRead(incomingByte,i));
-     sendBit(bitRead(incomingByte,i));
+    //send each bit seperately to serial monitor for debugging and to other arduino
+    for (int i = 7; i >= 0; i--)
+    {
+      Serial.print(bitRead(incomingByte, i));
+      sendBit(bitRead(incomingByte, i));
     }
-  Serial.println();
-  
+    Serial.println();
   }
-
-  
-  
 }
 
-void clock_interrupt_start () {
+//read incoming bits
+void clock_interrupt_start()
+{
   bool dataval = !digitalRead(reciever_data);
-  bitWrite( temp,recieve_index, dataval);
+  bitWrite(temp, recieve_index, dataval);
 }
 
-void clock_interrupt_end (){
-  unsigned long delta= millis() -last_millis;
-  if(delta>100){
-    b= temp;
+//verify incoming bit
+void clock_interrupt_end()
+{
+  //calculate delta since clock fell, to check for hazards
+  unsigned long delta = millis() - last_millis;
+  //if delta > 100ms, it's nit a hazard or glitch from the photoresistor
+  if (delta > 100)
+  {
+    //write temporary byte to "longterm" byte
+    b = temp;
+    //reduce recieve index to move to next bit
     recieve_index--;
-    if(recieve_index<0){
+    //if recieve_index < 0 we have successfully recieved a byte and print that to the serial monitor
+    if (recieve_index < 0)
+    {
       Serial.print(b);
+      //reset recieving variables to default state
       recieve_index = 7;
       b = 0x0;
     }
-    last_millis = millis (); 
+    //reset for delta caluclation
+    last_millis = millis();
   }
-  else{
-    temp =b;
+  else
+  {
+    //if a glitch was detected, delete temporarily recieved bit
+    temp = b;
   }
 }
 
-void sendBit (bool bitt){
-  
+//send single bit
+void sendBit(bool bitt)
+{
+  //change data pin
   digitalWrite(sender_data, bitt);
   delayMicroseconds(10);
+  //change clock to HIGH
   digitalWrite(sender_clock, HIGH);
+  //wait to be able to differentiate between valid bit and a hazard
   delayMicroseconds(500);
+  //change both pins to default state
   digitalWrite(sender_clock, LOW);
   digitalWrite(sender_data, LOW);
   delayMicroseconds(5);
-  }
+}
